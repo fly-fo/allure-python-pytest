413748d6e4dfc95a0971fd24ad2021247d354c34
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-30 12:15:30 +0100

    Add priority label to simple test case

--- test/pytest_quick_start_test.py
+++ test/pytest_quick_start_test.py
@@ -5,6 +5,7 @@ pytestmark = [allure.epic("My first epic"), allure.feature("Quick start feature"
 
 @allure.id(1)
 @allure.story("Simple story")
+@allure.label("priority", "Medium")
 @allure.title("test_allure_simple_test displayName")
 def test_allure_simple_test():
     with allure.step("Simple step"):
