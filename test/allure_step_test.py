042b81683ed88f047341bda5e81b11c42aef0171
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-30 12:46:29 +0100

    Update allure_step_test.py

--- test/allure_step_test.py
+++ test/allure_step_test.py
@@ -5,17 +5,17 @@ import allure
 def step_function_with_args(arg1, arg2):
     pass
 
-
+@allure.label("priority", "Medium")
 @allure.step("Step with param '{param}'")
 def step_with_placeholder(param):
     pass
 
-
+@allure.label("priority", "Medium")
 @allure.step("Custom step title")
 def step_function_with_title():
     pass
 
-
+@allure.label("priority", "Medium")
 def test_step():
     with allure.step("First step"):
         step_with_placeholder("Param value")
