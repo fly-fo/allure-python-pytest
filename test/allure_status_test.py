da92e25abb2e5102e0d7388215b7346baba3114e
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-30 12:45:57 +0100

    Update allure_status_test.py

--- test/allure_status_test.py
+++ test/allure_status_test.py
@@ -10,26 +10,26 @@ from allure import attachment_type
 def test_passed():
     pass
 
-
+@allure.label("priority", "Low")
 def test_failed():
     assert False, "Some fail reason"
 
-
+@allure.label("priority", "Low")
 def test_broken():
     var = 1 / 0
     assert var
 
-
+@allure.label("priority", "Medium")
 @pytest.mark.skip(reason="Some skip reason")
 def test_skipped():
     pass
 
-
+@allure.label("priority", "Low")
 @pytest.mark.xfail(reason="Some xfail reason")
 def test_xfail():
     assert False, "Assertion failed"
 
-
+@allure.label("priority", "Medium")
 @pytest.mark.xfail(reason="Test doesn't fail")
 def test_xpass():
     pass
